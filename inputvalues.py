215fd7a1df2a79e1f7ea466d9f06fbc9f6dfdf5d
552ayesha <82565617+552ayesha@users.noreply.github.com>
2021-04-15 14:28:29 +0530

    Create inputvalues.py

---
+++ inputvalues.py
@@ -0,0 +1,14 @@
+>>> a=input('enter first year friend')
+enter first year friendsaradha
+>>> b=input('enter second year friend')
+enter second year friendashu
+>>> c=input('enter third year friend')
+enter third year friendrani
+>>> print('first year, second year and third year friends are',a,b,c)
+first year, second year and third year friends are saradha ashu rani
+>>> print(type(a))
+<class 'str'>
+>>> print(type(b))
+<class 'str'>
+>>> print(type(c))
+<class 'str'>
