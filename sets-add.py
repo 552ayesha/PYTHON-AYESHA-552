1f531c0101adfad566243adec64f5ea30f30458f
sk ayesha <82565617+552ayesha@users.noreply.github.com>
2021-06-16 11:33:40 +0530

    Create sets-add.py

---
+++ sets-add.py
@@ -0,0 +1,18 @@
+N = int(input())
+countries = set()
+for i in range(N):
+    countries.add(input())
+print(len(countries))
+'''
+Input:
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+Output:
+5
+'''
