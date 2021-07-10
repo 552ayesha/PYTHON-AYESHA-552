eb31d8990a54c98bba5ecf4fe7fa12dfc7dbba2d
sk ayesha <82565617+552ayesha@users.noreply.github.com>
2021-07-10 15:19:25 +0530

    Create listaccess.py

---
+++ listaccess.py
@@ -0,0 +1,9 @@
+from listoperations import *
+print(list_sum([1,2,3,4]))
+print(list_sort([9,2,3,4,10]))
+print(string_list_conversions("hello"))
+"""
+10
+[2, 3, 4, 9, 10]
+['h', 'e', 'l', 'l', 'o']
+"""
